f710e44fc05c9a20ae39fe2e46482c82d9e0cb9e
Sage9705 <edem22kumahor@gmail.com>
2024-07-14 21:35:49 +0100

    update endpoint

--- pages/api/spotify/top-tracks.js
+++ pages/api/spotify/top-tracks.js
@@ -3,15 +3,20 @@ import { getSpotifyData } from "../../../lib/spotify";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
+
   if (!session) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
+  const { limit = 10, time_range = "long_term" } = req.query;
+
   try {
-    const data = await getSpotifyData(session.accessToken, '/me/top/tracks?limit=10');
-    res.status(200).json(data.items);
+    const topTracks = await getSpotifyData(
+      session.accessToken,
+      `/me/top/tracks?limit=${limit}&time_range=${time_range}`
+    );
+    res.status(200).json(topTracks.items);
   } catch (error) {
-    console.error("Error fetching top tracks", error);
     res.status(500).json({ error: "Error fetching top tracks" });
   }
 }
