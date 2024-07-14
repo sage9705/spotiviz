309bd64e10cfe09c9c83a3037847372d66f13454
Sage9705 <edem22kumahor@gmail.com>
2024-07-14 22:22:02 +0100

    configure external image sources

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.scdn.co', 'mosaic.scdn.co', 'seeded-session-images.scdn.co'],
+    domains: [
+      'i.scdn.co',
+      'mosaic.scdn.co',
+      'seeded-session-images.scdn.co',
+      'image-cdn-ak.spotifycdn.com',
+      'image-cdn-fa.spotifycdn.com'
+    ],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
